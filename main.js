5c7dd7e27fa27b628256f63647dc21e4ce0d1a1a
mtzz042 <160506117+mtzz042@users.noreply.github.com>
2024-06-17 11:47:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const muneroSenha = document.querySelector('parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.paremetro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector(',forca');
